a0c96eb8b50efa59cabcc395a2b4dbbd650cc263
M1K43L4 <cubon.mikaela@yahoo.com>
2019-11-15 22:46:56 -0600

    Add files via upload

---
+++ Learn How to Code/Intro_to_Variables.js
@@ -0,0 +1,29 @@
+//a variable is a way of saving a piece of information with a specific name. 
+//By giving a value a name, we can easily reuse that value over and over again in our code. 
+// variables are to:  reuse a value ; change a value
+
+my_number = 847595593392818109495
+my_number * 2
+my_number / 4
+
+//When we use a value without assigning it to a variable, that’s known as hardcoding. 
+//While it’s sometimes faster to initially hardcode values in your program, 
+//in the long run you’ll run into trouble — especially if you need to change what those values are.
+
+//We create or declare a variable by giving it a name and setting it equal to a value.
+
+// declare variable one
+// set it equal to the value 'grass'
+
+one = 'grass'
+
+
+// declare variable two
+// set it equal to the value 'rocks'
+
+two = 'rocks'
+
+// declare variable three
+// set it equal to the value 'forest'
+
+three = 'forest'
